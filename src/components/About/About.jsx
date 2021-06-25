3ca16da96888f672052e7461219f74f268812fe3
Soorya Kriz <sooryakriz111@gmail.com>
2021-06-25 07:13:26 +0530

    Images Assets Corrected

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import image from "./imageonline-co-grayscaledimage-2.jpg"
+import image from "./images/reading-book-about.jpg";
 
 export default function About() {
   return (
     <div className="w-full h-screen bg-l-col flex items-center pt-20">
       {/* Left Section */}
       <div className="w-1/2 flex flex-col justify-end items-center h-full relative">
-        <img className="w-3/4 absolute  top-0 -left-44" src={image} alt=""  />
+        <img className="w-3/4 absolute  top-0 -left-44" src={image} alt="" />
         <div className="w-99 font-Mulish text-lg text-light mb-28">
           <div className="w-full flex items-start justify-start">
             <span className="w-16 font-extrabold text-8xl -mt-6px">T</span>
@@ -25,7 +25,8 @@ export default function About() {
       {/* Right Section */}
       <div className="w-1/2 h-full pt-12">
         <span className="font-Mak-Bold text-9xl text-primary-col leading-tight">
-          The <br /> 19th <br /> Annual <br /> Book<br /> Festival
+          The <br /> 19th <br /> Annual <br /> Book
+          <br /> Festival
         </span>
       </div>
     </div>
